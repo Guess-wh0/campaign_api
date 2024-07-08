4ca088669f0bd2f4138a8af90608f48cdd0e13d2
ab rawat <abhisheksinghrawat.9@gmail.com>
2024-07-08 21:55:27 +0530

    fixed filter endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-
   def index
     @users = User.all
     render json: @users
@@ -9,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: { message: "Account successfully created", user: { user_id: @user.id, nickname: @user.name } }, status: :created
+      render json: { message: "Account successfully created", user: user.to_json }, status: :created
     else
       render json: { message: "Account creation failed", cause: @user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -17,19 +15,13 @@ class Api::V1::UsersController < ApplicationController
 
   def filter
     campaign_names = params[:campaign_names].split(',')
-    @users = User.where("JSON_CONTAINS(campaigns_list, ?)", campaign_names.to_json)
+    @users = User.where(
+      campaign_names.map {
+        |name| "JSON_CONTAINS(campaigns_list, '{\"campaign_name\": \"#{name}\"}', '$')"
+      }.join(' OR ')
+    )
+
     render json: @users
-  end
-
-  private
-
-  def set_user
-    @user = User.find_by(id: params[:id])
-    user_not_found unless @user
-  end
-
-  def user_not_found
-    render json: { message: "No User found" }, status: :not_found
   end
 
   def user_params
